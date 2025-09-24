f41487599d7ca21029adaf3762ba5e588ff407da
bscit <bscitdv@gmail.com>
2025-09-24 14:27:23 -0700

    init

---
+++ src/main/java/dev/bscit/item_labels/Main.java
@@ -0,0 +1,10 @@
+package dev.bscit.item_labels;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/dev/bscit/item_labels/client/MainClient.java
@@ -0,0 +1,10 @@
+package dev.bscit.item_labels.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
