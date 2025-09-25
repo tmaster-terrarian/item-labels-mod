2ffd1f6e4d489d4dd48d60290c34902c12812335
bscit <bscitdv@gmail.com>
2025-09-24 21:50:42 -0700

    1.21.6.. and onward hopefully!

--- src/main/java/dev/bscit/item_labels/components/ItemLabelsComponents.java
+++ src/main/java/dev/bscit/item_labels/components/ItemLabelsComponents.java
@@ -6,6 +6,9 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+//? if >=1.21.6
+/*import net.fabricmc.fabric.api.item.v1.ComponentTooltipAppenderRegistry;*/
+
 public class ItemLabelsComponents {
     public static final ComponentType<LabelsComponent> LABELS = ComponentType.<LabelsComponent>builder()
             .codec(LabelsComponent.CODEC)
@@ -14,5 +17,9 @@ public class ItemLabelsComponents {
     public static void Initialize()
     {
         Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of(ItemLabels.ID, "labels"), LABELS);
+
+        /*? if >=1.21.6 {*/
+        /*ComponentTooltipAppenderRegistry.addFirst(LABELS);
+        *//*?}*/
     }
 }
